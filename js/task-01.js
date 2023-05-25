7605130970ad0adde06dc80c09fec3c99319eea8
alexandr-ni <sashanikitenko119@gmail.com>
2023-05-25 18:14:59 +0300

    bugfix-firsthomework

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,9 @@ const items = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${items.length}`);
 items.forEach((item) => {
-  const title = item.querySelector("h2");
+  const title = item.firstElementChild;
   console.log(`Category: ${title.textContent}`);
 
-  const itemEl = item.querySelectorAll("li");
+  const itemEl = item.firstElementChild.nextElementSibling.children;
   console.log(`Elements: ${itemEl.length}`);
 });
